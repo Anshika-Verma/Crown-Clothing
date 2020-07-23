b86cab250a6967f65d99e199db5c1524d356e34d
Anshika Verma <anshikaverma558@gmail.com>
2020-07-24 02:50:01 +0530

    Added cart-icon & car dropdown components, also implemented cart reducer

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { userActionTypes } from './user.types';
+import { UserActionTypes } from './user.types';
 
 export const setCurrentUser = user => ({
   type: UserActionTypes.SET_CURRENT_USER,
